68f30ec9a997174dd654a1b810927ac98d2f2195
Vallabh Joshi <68896675+Vallabhjoshi1998@users.noreply.github.com>
2021-12-30 11:26:09 +0530

    Added admin logo

--- source/modules/admin/admin_dash_header.php
+++ source/modules/admin/admin_dash_header.php
@@ -267,7 +267,7 @@ $row_admin_name = mysqli_fetch_array($result);
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <!-- <span class="mr-2 d-none d-lg-inline text-gray-600 small">User Name</span> -->
-                                <img class="img-profile rounded-circle" src="../../assets/img/favicon.png">
+                                <img class="img-profile rounded-circle" src="../../assets/img/admin.png">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
